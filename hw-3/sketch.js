570b8b0c3b7e10d42356a8a9f215805b2ee1e6fe
Lauren Fordham <lauren.fordham@umontana.edu>
2018-09-11 21:28:31 -0600

    Homework

---
+++ hw-3/sketch.js
@@ -0,0 +1,11 @@
+//select setup function
+function setup(){
+  //create canvas 800px by 500px
+    createCanvas(800, 500)
+    //color the background 'yellow'
+    background('yellow');
+}
+function draw() {
+
+text("Hello World!", 20, 30);
+}
